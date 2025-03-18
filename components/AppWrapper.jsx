2a1ae7f1bd34ef807f337ad47021bbe75472effb
musaabMD <mousab.r@outlook.com>
2025-03-18 23:51:54 +0300

    “hi”

--- components/AppWrapper.jsx
+++ components/AppWrapper.jsx
@@ -1,15 +1,15 @@
-// File: src/components/AppWrapper.jsx
-'use client'
-import React from 'react';
-import { AuthProvider } from '@/hooks/useAuth';
-import MobileExamApp from './MobileExamApp';
+// // File: src/components/AppWrapper.jsx
+// 'use client'
+// import React from 'react';
+// import { AuthProvider } from '@/hooks/useAuth';
+// import MobileExamApp from './MobileExamApp';
 
-const AppWrapper = () => {
-  return (
-    <AuthProvider>
-      <MobileExamApp />
-    </AuthProvider>
-  );
-};
+// const AppWrapper = () => {
+//   return (
+//     <AuthProvider>
+//       <MobileExamApp />
+//     </AuthProvider>
+//   );
+// };
 
-export default AppWrapper;
+// export default AppWrapper;
